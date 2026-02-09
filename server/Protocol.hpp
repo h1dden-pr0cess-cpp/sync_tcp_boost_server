cfba8c2df050189bf7253cb8d1a27c12c34e82f3
h1dden_pr0cess-cpp <schwenkmark2007@gmail.com>
2026-02-09 22:52:49 +0500

    new protocol

--- server/Protocol.hpp
+++ server/Protocol.hpp
@@ -5,11 +5,40 @@
 
 enum class PacketType : uint8_t
 {
-    Ping        = 1,
-    UploadChunk = 2,
-    Download    = 3,
-    ListFiles   = 4
+    Ping             = 1,
+    UploadChunk      = 2,
+    Download         = 3,
+    ListFiles        = 4,
+
+    RegisterUser     = 10, 
+    LoginWithPassword= 11, 
+    LoginWithToken   = 12, 
+    Logout           = 13  
 };
+
+
+struct AuthPasswordPacket
+{
+    std::string username;
+    std::string password_hash; 
+};
+
+struct AuthTokenPacket
+{
+    std::string token; 
+};
+
+struct RegisterPacket
+{
+    std::string username;
+    std::string password_hash;
+};
+
+struct AuthResponsePacket
+{
+    bool success;
+    std::string message; 
+	std::string token;  };
 
 #endif // PROTOCOL_HPP
 
